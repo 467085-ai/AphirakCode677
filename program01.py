f2a2aad37b682d2e9e32f788f17d873e4d2825e2
467085-ai <467085@wsk.ac.th>
2026-06-17 02:50:50 +0000

    program01.py

---
+++ program01.py
@@ -0,0 +1,8 @@
+print("hello bro")
+print(".........")
+num1=float(input("enter number1;"))
+num2=int(input("enter number2;"))
+sum=num1+num2
+print("total is:",sum)
+print("ผู้พัฒนาโปรแกรม คือ ครูสุมามาลย์")
+
