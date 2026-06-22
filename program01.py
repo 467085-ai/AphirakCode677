853167895ea593b7efcbcb8ea7801f260d699983
467085-ai <467085@wsk.ac.th>
2026-06-22 21:20:31 +0700

    Update program01.py

--- program01.py
+++ program01.py
@@ -1,8 +1,6 @@
-print("hello bro")
-print(".........")
-num1=float(input("enter number1;"))
-num2=int(input("enter number2;"))
-sum=num1+num2
-print("total is:",sum)
-print("ผู้พัฒนาโปรแกรม คือ ครูสุมามาลย์")
-
+print("โปรเเกรมหาพื้นที่สามเหลี่ยม")
+print(".....................")
+tall = float(input("enter hieght"))
+base = float(input("enter base"))
+areatri = 0.5*tall*base
+print("พื้นที่สามเหลี่ยม =",areatri)
