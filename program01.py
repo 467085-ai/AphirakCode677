cc9525dc0dc75b057303c646233f7bd765385499
467085-ai <467085@wsk.ac.th>
2026-06-24 08:20:29 +0700

    Update program01.py

--- program01.py
+++ program01.py
@@ -4,3 +4,4 @@ tall = float(input("enter hieght"))
 base = float(input("enter base"))
 areatri = 0.5*tall*base
 print("พื้นที่สามเหลี่ยม =",areatri)
+print("ผู้พัฒนาโปรเเกรม โดยนายอภิรักษ์ พิมพ์ทอง")
